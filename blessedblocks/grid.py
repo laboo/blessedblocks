58646cb91eb5b99d7c35fe6bccafbd6475bed63b
Mark Libucha <mlibucha@gmail.com>
2018-06-06 20:45:23 -0700

    Added beginnings of command line input.

--- blessedblocks/grid.py
+++ blessedblocks/grid.py
@@ -39,10 +39,17 @@ class Grid(object):
             args=()
         )
 
+        self._input = Thread(
+            name='input',
+            target=self._input,
+            args=()
+        )
+
         signal.signal(signal.SIGWINCH, self._on_resize)
         signal.signal(signal.SIGINT, self._on_kill)
 
         self._thread.start()
+        self._input.start()
 
     def stop(self, *args):
         self._term.clear()
@@ -51,6 +58,8 @@ class Grid(object):
             self._refresh.set() # in order to release it from a wait()
             if self._thread and self._thread.isAlive():
                 self._thread.join()
+            if self._input and self._input.isAlive():
+                self._input.join()
 
     def done(self):
         return not self._thread.isAlive() or self._done.is_set()
@@ -61,27 +70,47 @@ class Grid(object):
             while True:
                 if self._done.is_set():
                     break
-                self._refresh.wait()
+                if not self._refresh.wait(.5):
+                    continue
                 with self._lock:
                     if self._not_just_dirty.is_set():
                         just_dirty = False
                         self._not_just_dirty.clear()
                     else:
                         just_dirty = True
-                    self._block.display(self._term.width, self._term.height, 0, 0, self._term, just_dirty=just_dirty)
-                    self._term.move(0,0)
+                    self._block.display(self._term.width, self._term.height-1, 0, 0, self._term, just_dirty=just_dirty)
                     self._refresh.clear()
 
     def update(self):
-        with self._lock:
-            self._refresh.set()
+        self._refresh.set()
 
     def update_block(self, index, block):
         with self._lock:
             self._block.arrangement._slots[index] = deepcopy(block)
         self.update()
-            
+
     def load(self, arrangement):
         with self._lock:
             self._block.arrangement = deepcopy(arrangement)
         self.update_all()
+
+    def _input(self):
+        with self._term.cbreak():
+            val = ''
+            while val.lower() != 'q':
+                print(self._term.move(self._term.height, 2) + '', end='')
+                val = self._term.inkey(timeout=.5)
+                if self._done.is_set():
+                    break
+                with self._lock:
+                    if not val:
+                        # timeout
+                        with self._term.location(x=0, y=self._term.height):
+                            print('> ' + (' '  * (self._term.width - 2)), end='')
+                            continue
+                    elif val.is_sequence:
+                        with self._term.location(x=2, y=self._term.height-1):
+                            print("got sequence: {0}.".format((str(val), val.name, val.code)), end='')
+                    elif val:
+                        with self._term.location(x=2, y=self._term.height-1):
+                            print("got {0}.".format(val), end='')
